9771c57983eebb1cc3a7fb1469a3e6e4e864ce1e
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-01-16 17:52:12 +0800

    Added sample balance display

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,42 @@ import './App.css'
 
 import Web3 from 'web3'
 import Wallet from './components/Wallet'
+import { TOKEN_ADDRESS } from './config'
 
 function App() {
 
 	const [ account, setAccount ] = useState('')
+	const [ balance, setBalance ] = useState('')
 
 	const loadBlockchainData = async() => {
 		const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545')
+
+		// Account 
 		const accounts = await web3.eth.getAccounts()
 		setAccount(accounts[0])
+
+		// Balance
+		const contract = new web3.eth.Contract([
+			// balanceOf
+			{
+				'constant':true,
+				'inputs':[{'name':'_owner','type':'address'}],
+				'name':'balanceOf',
+				'outputs':[{'name':'balance','type':'uint256'}],
+				'type':'function'
+			},
+			// decimals
+			{
+				'constant':true,
+				'inputs':[],
+				'name':'decimals',
+				'outputs':[{'name':'','type':'uint8'}],
+				'type':'function'
+			}
+		],TOKEN_ADDRESS)
+
+		const balanceInWei = await contract.methods.balanceOf(accounts[0]).call()
+		setBalance(web3.utils.fromWei(balanceInWei.toString()).toString())
 	}
 
 	useEffect(() => {
@@ -20,7 +47,7 @@ function App() {
 
 	return (
 		<div className="App">
-			<Wallet wallet_address={account} />
+			<Wallet balance={ balance? balance:'0' } wallet_address={account} />
 		</div>
 	)
 }
---
+++ src/config/index.ts
@@ -0,0 +1,1 @@
+export const TOKEN_ADDRESS = '0x8c38A1C63e3e8e0b11d616f4DFFfbB1c342aCEFc'
