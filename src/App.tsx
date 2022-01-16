6aba64d6080d9273c4a569d72b12a43dc30573c7
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-01-16 16:45:35 +0800

    Lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 import Web3 from 'web3'
-import Wallet from './components/Wallet';
+import Wallet from './components/Wallet'
 
 function App() {
 
-  const [ account, setAccount ] = useState('')
+	const [ account, setAccount ] = useState('')
 
-  const loadBlockchainData = async() => {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
-    const accounts = await web3.eth.getAccounts()
-    setAccount(accounts[0])
-  }
+	const loadBlockchainData = async() => {
+		const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545')
+		const accounts = await web3.eth.getAccounts()
+		setAccount(accounts[0])
+	}
 
-  useEffect(() => {
-    loadBlockchainData()
-  }, [])
+	useEffect(() => {
+		loadBlockchainData()
+	}, [])
 
-  return (
-    <div className="App">
-      <Wallet wallet_address={account} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Wallet wallet_address={account} />
+		</div>
+	)
 }
 
-export default App;
+export default App
