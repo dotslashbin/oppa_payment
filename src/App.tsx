ec032044568f33dc72a42b85ba3c9b214c7161aa
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-03-13 20:22:29 +0800

    Cleaned up

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import './App.css'
 
 import Web3 from 'web3'
 import Wallet from './components/Wallet'
-import { TOKEN_ADDRESS } from './config'
 import Payment from './components/Payment'
+import { OPPAtoken, web3Provider } from './contract'
 
 function App() {
 
 	const [ account, setAccount ] = useState('')
 	const [ balance, setBalance ] = useState('')
-
-	const web3Provider = new Web3(Web3.givenProvider || 'http://localhost:8545')
 
 	const loadBlockchainData = async() => {
 
@@ -19,27 +17,7 @@ function App() {
 		const accounts = await web3Provider.eth.getAccounts()
 		setAccount(accounts[0])
 
-		// Balance
-		const contract = new web3Provider.eth.Contract([
-			// balanceOf
-			{
-				'constant':true,
-				'inputs':[{'name':'_owner','type':'address'}],
-				'name':'balanceOf',
-				'outputs':[{'name':'balance','type':'uint256'}],
-				'type':'function'
-			},
-			// decimals
-			{
-				'constant':true,
-				'inputs':[],
-				'name':'decimals',
-				'outputs':[{'name':'','type':'uint8'}],
-				'type':'function'
-			}
-		],TOKEN_ADDRESS)
-
-		const balanceInWei = await contract.methods.balanceOf(accounts[0]).call()
+		const balanceInWei = await OPPAtoken.methods.balanceOf(accounts[0]).call()
 		setBalance(web3Provider.utils.fromWei(balanceInWei, 'gwei').toString())
 	}
 
@@ -50,7 +28,7 @@ function App() {
 	return (
 		<div className="App">
 			<Wallet balance={ balance? balance:'0' } wallet_address={ account } />
-			<Payment wallet_address={ account }/>
+			<Payment account={ account } />
 		</div>
 	)
 }
