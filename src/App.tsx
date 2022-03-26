58a06f74a107f99ea3ec278851e1e7c1975676f0
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-03-27 01:14:32 +0800

    Implemented connect button

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,52 @@ import Wallet from './components/Wallet'
 import Payment from './components/Payment'
 import { OPPAtoken, web3Provider } from './contract'
 
+
+declare global {
+	interface Window {
+			ethereum: any
+	}
+}
+
+function isMetaMaskInstalled() {
+	return Boolean(window.ethereum && window.ethereum.isMetaMask)
+}
+
+function isMetaMaskConnected() {
+	const {ethereum} = window
+	const accounts = ethereum.request({method: 'eth_accounts'})
+	return accounts && accounts.length > 0
+}
+
 function App() {
 
 	const [ account, setAccount ] = useState('')
 	const [ balance, setBalance ] = useState('')
+	const [ connected, setConnected ] = useState(false)
+
 
 	const loadBlockchainData = async() => {
-
 		// Account 
 		const accounts = await web3Provider.eth.getAccounts()
 		setAccount(accounts[0])
+		setConnected(true)
 
 		const balanceInWei = await OPPAtoken.methods.balanceOf(accounts[0]).call()
 		setBalance(web3Provider.utils.fromWei(balanceInWei, 'gwei').toString())
 	}
 
-	useEffect(() => {
-		loadBlockchainData()
-	}, [])
-
 	return (
 		<div className="App">
-			<Wallet balance={ balance? balance:'0' } wallet_address={ account } />
-			<Payment account={ account } />
+			{ !connected && !isMetaMaskConnected()? (<button onClick={() => {
+				const { ethereum } = window
+				ethereum.request({ method: 'eth_requestAccounts' })
+				loadBlockchainData()
+
+			}}>Connect Metamask</button>) : (
+				<>
+					<Wallet balance={ balance? balance:'0' } wallet_address={ account } />
+					<Payment account={ account } /></>
+			)}
 		</div>
 	)
 }
